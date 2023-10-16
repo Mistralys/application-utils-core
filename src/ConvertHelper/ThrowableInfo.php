db688b93289c53f4df2dba3ec452bb05e44f5c3d
Mistralys <s.mordziol@gmail.com>
2023-10-16 17:05:43 +0200

    Restored, to allow handling the deprecation.

---
+++ src/ConvertHelper/ThrowableInfo.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package AppUtils
+ * @subpackage ThrowableInfo
+ * @see ThrowableInfo
+ */
+
+declare(strict_types=1);
+
+use AppUtils\ThrowableInfo;
+
+/**
+ * DEPRECATED: Use {@see ThrowableInfo} instead.
+ *
+ * @package AppUtils
+ * @subpackage ThrowableInfo
+ *
+ * @deprecated Use {@see ThrowableInfo} instead.
+ */
+class ConvertHelper_ThrowableInfo extends ThrowableInfo
+{
+
+}
