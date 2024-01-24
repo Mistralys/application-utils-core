ab4bb008960005b66b585b01e69e1f574fa60c4d
Mistralys <s.mordziol@gmail.com>
2024-01-24 09:50:00 +0100

    Added tests.

--- tests/AppUtilsTests/JSHelper/QuoteConversionTests.php
+++ tests/AppUtilsTests/JSHelper/QuoteConversionTests.php
@@ -22,7 +22,7 @@ EOT;
         $this->assertSame($expected, JSHelper::quoteStyle($source)->singleToDouble());
     }
 
-    public function test_singleToDoubleNested() : void
+    public function test_singleToDoubleMixed() : void
     {
         $source = <<<'EOT'
 alert('Some "single" text');
@@ -35,7 +35,7 @@ EOT;
         $this->assertSame($expected, JSHelper::quoteStyle($source)->singleToDouble());
     }
 
-    public function test_singleToDoubleNestedPreserved() : void
+    public function test_singleToDoubleMixedPreserved() : void
     {
         $source = <<<'EOT'
 alert('Some "single" text');
@@ -44,6 +44,30 @@ EOT;
         $expected = <<<'EOT'
 alert("Some 'single' text");
 EOT;
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->setPreserveMixed(true)
+                ->singleToDouble()
+        );
+    }
+
+    public function test_singleToDoubleEscaped() : void
+    {
+        $source = <<<'EOT'
+alert('Some \'single\' text');
+EOT;
+
+        $expected = <<<'EOT'
+alert("Some \"single\" text");
+EOT;
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->singleToDouble()
+        );
 
         $this->assertSame(
             $expected,
@@ -66,7 +90,7 @@ EOT;
         $this->assertSame($expected, JSHelper::quoteStyle($source)->doubleToSingle());
     }
 
-    public function test_doubleToSingleNested() : void
+    public function test_doubleToSingleMixed() : void
     {
         $source = <<<'EOT'
 alert("Some 'single' text");
@@ -79,7 +103,7 @@ EOT;
         $this->assertSame($expected, JSHelper::quoteStyle($source)->doubleToSingle());
     }
 
-    public function test_doubleToSingleNestedPreserved() : void
+    public function test_doubleToSingleMixedPreserved() : void
     {
         $source = <<<'EOT'
 alert("Some 'single' text");
@@ -93,6 +117,86 @@ EOT;
             $expected,
             JSHelper::quoteStyle($source)
                 ->setPreserveMixed(true)
+                ->doubleToSingle()
+        );
+    }
+
+    public function test_doubleToSingleEscaped() : void
+    {
+        $source = <<<'EOT'
+alert("Some \"single\" text");
+EOT;
+
+        $expected = <<<'EOT'
+alert('Some \'single\' text');
+EOT;
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->doubleToSingle()
+        );
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->setPreserveMixed(true)
+                ->doubleToSingle()
+        );
+    }
+
+    public function test_doubleToSingleWithHTML() : void
+    {
+        $source = <<<'EOT'
+alert("Some <span title=\"single\">text</span> and <strong>bold</strong> and <a href=\"#\">Link</a>.");
+EOT;
+
+        $expected = <<<'EOT'
+alert('Some <span title="single">text</span> and <strong>bold</strong> and <a href="#">Link</a>.');
+EOT;
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->doubleToSingle()
+        );
+    }
+
+    public function test_singleToDoubleWithHTML() : void
+    {
+        $source = <<<'EOT'
+alert('Some <span title="single">text</span> and <strong>bold</strong> and <a href="#">Link</a>.');
+EOT;
+
+        $expected = <<<'EOT'
+alert("Some <span title=\"single\">text</span> and <strong>bold</strong> and <a href=\"#\">Link</a>.");
+EOT;
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->singleToDouble()
+        );
+    }
+
+    /**
+     * With HTML compatibility turned off, the HTML attribute quotes are
+     * converted as if they were part of the JS string.
+     */
+    public function test_singleToDoubleNoHTMLCompatibility() : void
+    {
+        $source = <<<'EOT'
+alert("Some <span title=\"single\">text</span> and <strong>bold</strong> and <a href=\"#\">Link</a>.");
+EOT;
+
+        $expected = <<<'EOT'
+alert('Some <span title=\'single\'>text</span> and <strong>bold</strong> and <a href=\'#\'>Link</a>.');
+EOT;
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->setHTMLCompatible(false)
                 ->doubleToSingle()
         );
     }
