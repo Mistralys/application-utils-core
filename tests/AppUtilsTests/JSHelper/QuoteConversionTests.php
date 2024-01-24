318142bc4b152103dd23d2eb0bd2c72b8797478d
Mistralys <s.mordziol@gmail.com>
2024-01-24 13:54:01 +0100

    Added tests.

--- tests/AppUtilsTests/JSHelper/QuoteConversionTests.php
+++ tests/AppUtilsTests/JSHelper/QuoteConversionTests.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace AppUtilsTests\JSHelper;
 
 use AppUtils\JSHelper;
+use AppUtils\JSHelper\QuoteConverter;
 use AppUtilsTestClasses\BaseTestCase;
 
 final class QuoteConversionTests extends BaseTestCase
@@ -17,6 +18,19 @@ EOT;
 
         $expected = <<<'EOT'
 alert("Some text");
+EOT;
+
+        $this->assertSame($expected, JSHelper::quoteStyle($source)->singleToDouble());
+    }
+
+    public function test_multipleStatements() : void
+    {
+        $source = <<<'EOT'
+alert('Some text');alert("Another text");
+EOT;
+
+        $expected = <<<'EOT'
+alert("Some text");alert("Another text");
 EOT;
 
         $this->assertSame($expected, JSHelper::quoteStyle($source)->singleToDouble());
@@ -85,6 +99,19 @@ EOT;
 
         $expected = <<<'EOT'
 alert('Some text');
+EOT;
+
+        $this->assertSame($expected, JSHelper::quoteStyle($source)->doubleToSingle());
+    }
+
+    public function test_doubleToSingleIfAlreadySingle() : void
+    {
+        $source = <<<'EOT'
+alert('Some "quoted" text');
+EOT;
+
+        $expected = <<<'EOT'
+alert('Some \'quoted\' text');
 EOT;
 
         $this->assertSame($expected, JSHelper::quoteStyle($source)->doubleToSingle());
@@ -160,6 +187,13 @@ EOT;
             JSHelper::quoteStyle($source)
                 ->doubleToSingle()
         );
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->setPreserveMixed(true)
+                ->doubleToSingle()
+        );
     }
 
     public function test_singleToDoubleWithHTML() : void
@@ -177,13 +211,20 @@ EOT;
             JSHelper::quoteStyle($source)
                 ->singleToDouble()
         );
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->setPreserveMixed(true)
+                ->singleToDouble()
+        );
     }
 
     /**
      * With HTML compatibility turned off, the HTML attribute quotes are
      * converted as if they were part of the JS string.
      */
-    public function test_singleToDoubleNoHTMLCompatibility() : void
+    public function test_doubleToSingleNoHTMLCompatibility() : void
     {
         $source = <<<'EOT'
 alert("Some <span title=\"single\">text</span> and <strong>bold</strong> and <a href=\"#\">Link</a>.");
@@ -199,5 +240,61 @@ EOT;
                 ->setHTMLCompatible(false)
                 ->doubleToSingle()
         );
+    }
+
+    public function test_brokenQuotes() : void
+    {
+        $source = <<<'EOT'
+alert('Broken");
+EOT;
+
+        $this->expectExceptionCode(QuoteConverter::ERROR_BROKEN_QUOTES);
+
+        JSHelper::quoteStyle($source)->singleToDouble();
+    }
+
+    public function test_validSingleQuoteInAttribute() : void
+    {
+        $source = <<<'EOT'
+alert("Broken <span title=\"Title's bad\">attribute</span>.");
+EOT;
+
+        $expected = <<<'EOT'
+alert('Broken <span title="Title\'s bad">attribute</span>.');
+EOT;
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->doubleToSingle()
+        );
+    }
+
+    public function test_validSingleToDoubleQuoteInAttribute() : void
+    {
+        $source = <<<'EOT'
+alert('Broken <span title="Title\'s bad">attribute</span>.');
+EOT;
+
+        $expected = <<<'EOT'
+alert("Broken <span title=\"Title's bad\">attribute</span>.");
+EOT;
+
+        $this->assertSame(
+            $expected,
+            JSHelper::quoteStyle($source)
+                ->singleToDouble()
+        );
+    }
+
+    public function test_brokenHTMLAttributeQuote() : void
+    {
+        $source = <<<'EOT'
+alert('Broken <span title="Title\'s bad\">attribute</span>');
+EOT;
+
+        $this->expectExceptionCode(QuoteConverter::ERROR_MISMATCHED_ATTRIBUTE_QUOTE);
+
+        JSHelper::quoteStyle($source)->singleToDouble();
     }
 }
