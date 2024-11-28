ce88590577c42e3517a7c831e7269930cab83016
Mistralys <s.mordziol@gmail.com>
2024-11-28 16:05:23 +0100

    Added `formatHTML()`.

--- src/HTMLHelper.php
+++ src/HTMLHelper.php
@@ -11,6 +11,9 @@ declare(strict_types=1);
 
 namespace AppUtils;
 
+use AppUtils\HTMLHelper\HTMLHelperException;
+use DOMDocument;
+
 /**
  * Helper for common HTML-related tasks.
  *
@@ -43,7 +46,7 @@ class HTMLHelper
 
     /**
      * Injects the target text at the end of an HTML snippet,
-     * either in an existing <p> tag, or in a new <p> tag if
+     * either in an existing <p> tag or in a new <p> tag if
      * the last block tag cannot be used (<ul> for example).
      *
      * NOTE: Assumes that it is not a whole HTML document.
@@ -51,6 +54,7 @@ class HTMLHelper
      * @param string $text
      * @param string $html
      * @return string
+     * @throws HTMLHelperException {@see HTMLHelperException::ERROR_CANNOT_FIND_CLOSING_TAG}
      */
     public static function injectAtEnd(string $text, string $html) : string
     {
@@ -63,6 +67,14 @@ class HTMLHelper
         $tagName = array_pop($result[1]);
         $pos = strrpos($html, '</'.$tagName.'>');
 
+        if($pos === false) {
+            throw new HTMLHelperException(
+                'Could not find closing tag for ['.$tagName.'].',
+                '',
+                HTMLHelperException::ERROR_CANNOT_FIND_CLOSING_TAG
+            );
+        }
+
         if(in_array(strtolower($tagName), self::$newParaTags)) {
             $replace = '</'.$tagName.'><p>'.$text.'</p>';
         } else {
@@ -70,5 +82,33 @@ class HTMLHelper
         }
 
         return (string)substr_replace($html, $replace, $pos, strlen($html));
+    }
+
+    /**
+     * Formats the HTML to make it readable.
+     *
+     * @param string $html
+     * @return string
+     * @throws HTMLHelperException {@see HTMLHelperException::ERROR_FORMAT_HTML_FAILED}
+     */
+    public static function formatHTML(string $html) : string
+    {
+        $dom = new DOMDocument();
+        $dom->preserveWhiteSpace = false;
+        $dom->formatOutput = true;
+
+        $dom->loadHTML($html);
+
+        $html = $dom->saveHTML();
+
+        if(is_string($html)) {
+            return $html;
+        }
+
+        throw new HTMLHelperException(
+            'Failed to format the HTML.',
+            '',
+            HTMLHelperException::ERROR_FORMAT_HTML_FAILED
+        );
     }
 }
