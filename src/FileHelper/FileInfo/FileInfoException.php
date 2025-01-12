4a260dfd9d0cfa1490b2f57385bf6937b76098db
Mistralys <s.mordziol@gmail.com>
2025-01-12 11:30:01 +0100

    Added an exception flavor.

---
+++ src/FileHelper/FileInfo/FileInfoException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @package FileHelper
+ * @subpackage FileInfo
+ */
+
+declare(strict_types=1);
+
+namespace AppUtils\FileHelper\FileInfo;
+
+use AppUtils\FileHelper_Exception;
+
+/**
+ * @package FileHelper
+ * @subpackage FileInfo
+ */
+class FileInfoException extends FileHelper_Exception
+{
+    public const ERROR_INVALID_OVERRIDE_CLASS = 170401;
+}
