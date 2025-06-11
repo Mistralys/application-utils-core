0146e113f18c668801be0741232bbf5cc991f103
Mistralys <s.mordziol@gmail.com>
2025-06-11 16:54:17 +0200

    Added error codes.

--- src/XMLHelper/Exception.php
+++ src/XMLHelper/Exception.php
@@ -7,4 +7,6 @@ namespace AppUtils;
 class XMLHelper_Exception extends BaseException
 {
     public const ERROR_FAILED_TO_FORMAT_XML = 168801;
+    public const ERROR_HTML_CONVERSION_FAILED = 168802;
+    public const ERROR_XML_CONVERSION_FAILED = 168803;
 }
