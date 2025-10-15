36f11b3835ad22192dfb2977e6a97ec4992ec7a2
Mistralys <s.mordziol@gmail.com>
2025-10-15 16:19:37 +0200

    Added array data collection tests.

--- tests/AppUtilsTests/ArrayDataCollection/CollectionTests.php
+++ tests/AppUtilsTests/ArrayDataCollection/CollectionTests.php
@@ -467,11 +467,123 @@ class CollectionTests extends BaseTestCase
             'array' => array('data' => 'here')
         ));
 
-        $this->assertSame(null, $collection->getStringN('null'));
-        $this->assertSame(null, $collection->getStringN('empty-string'));
+        $this->assertNull($collection->getStringN('null'));
+        $this->assertNull($collection->getStringN('empty-string'));
         $this->assertSame('0', $collection->getStringN('zero'));
         $this->assertSame('0', $collection->getStringN('zero-string'));
-        $this->assertSame(null, $collection->getStringN('array'));
+        $this->assertNull($collection->getStringN('array'));
+    }
+
+    public function test_pushArrayNotExists() : void
+    {
+        $collection = ArrayDataCollection::create();
+
+        $collection->setArray('items')->pushIndexed('first');
+
+        $this->assertSame(array('first'), $collection->getArray('items'));
+    }
+
+    public function test_pushArrayExists() : void
+    {
+        $collection = ArrayDataCollection::create(array(
+            'items' => array('first')
+        ));
+
+        $collection->setArray('items')->pushIndexed('second');
+
+        $this->assertSame(array('first','second'), $collection->getArray('items'));
+    }
+
+    public function test_setArrayKeyNotExists() : void
+    {
+        $collection = ArrayDataCollection::create();
+
+        $collection->setArray('items')->setAssoc('key', 'value');
+
+        $this->assertSame(array('key' => 'value'), $collection->getArray('items'));
+    }
+
+    public function test_setArrayKeyExists() : void
+    {
+        $collection = ArrayDataCollection::create(array(
+            'items' => array('key1' => 'value1')
+        ));
+
+        $collection->setArray('items')->setAssoc('key2', 'value2');
+
+        $this->assertSame(array('key1' => 'value1', 'key2' => 'value2'), $collection->getArray('items'));
+    }
+
+    public function test_removeArrayKey() : void
+    {
+        $collection = ArrayDataCollection::create(array(
+            'items' => array('key1' => 'value1')
+        ));
+
+        $collection->setArray('items')->removeAssoc('key1');
+
+        $this->assertSame(array(), $collection->getArray('items'));
+    }
+
+    public function test_clearArray() : void
+    {
+        $collection = ArrayDataCollection::create(array(
+            'items' => array('key1' => 'value1')
+        ));
+
+        $collection->setArray('items')->clear();
+
+        $this->assertSame(array(), $collection->getArray('items'));
+    }
+
+    public function test_shiftIndexedArray() : void
+    {
+        $collection = ArrayDataCollection::create(array(
+            'items' => array('first', 'second', 'third')
+        ));
+
+        $this->assertSame('first', $collection->setArray('items')->shiftIndexed());
+        $this->assertSame(array('second', 'third'), $collection->getArray('items'));
+
+        $this->assertSame('second', $collection->setArray('items')->shiftIndexed());
+        $this->assertSame(array('third'), $collection->getArray('items'));
+
+        $this->assertSame('third', $collection->setArray('items')->shiftIndexed());
+        $this->assertSame(array(), $collection->getArray('items'));
+
+        $this->assertNull($collection->setArray('items')->shiftIndexed());
+        $this->assertSame(array(), $collection->getArray('items'));
+    }
+
+    public function test_unshiftIndexArray() : void
+    {
+        $collection = ArrayDataCollection::create(array(
+            'items' => array('second', 'third')
+        ));
+
+        $collection->setArray('items')->unshiftIndexed('first');
+
+        $this->assertSame(array('first', 'second', 'third'), $collection->getArray('items'));
+
+        $collection->setArray('items')->unshiftIndexed('zero');
+
+        $this->assertSame(array('zero', 'first', 'second', 'third'), $collection->getArray('items'));
+
+    }
+
+    public function test_clearKeys() : void
+    {
+        $collection = ArrayDataCollection::create(array(
+            'foo' => 'bar',
+            'int' => 42
+        ));
+
+        $this->assertTrue($collection->keyExists('foo'));
+        $this->assertTrue($collection->keyExists('int'));
+
+        $collection->clearKeys();
+
+        $this->assertSame(array(), $collection->getData());
     }
 
     // endregion
